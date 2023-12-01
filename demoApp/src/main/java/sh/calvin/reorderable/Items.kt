359cd6a61a0d3ea23f06dfd7103b535f5976a99a
Calvin Liang <me@calvin.sh>
2023-12-01 14:30:01 -0800

    remove uuid from demo app items

--- demoApp/src/main/java/sh/calvin/reorderable/Items.kt
+++ demoApp/src/main/java/sh/calvin/reorderable/Items.kt
@@ -1,9 +1,7 @@
 package sh.calvin.reorderable
 
-import java.util.UUID
-
-data class Item(val id: UUID = UUID.randomUUID(), val text: String, val size: Int)
+data class Item(val id: Int, val text: String, val size: Int)
 
 val items = (0..200).map {
-    Item(text = "Item #$it", size = if (it % 2 == 0) 50 else 100)
+    Item(id = it, text = "Item #$it", size = if (it % 2 == 0) 50 else 100)
 }
