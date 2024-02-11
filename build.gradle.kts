473c3ba698b659c8f7440231e589629bc94482b0
Calvin Liang <me@calvin.sh>
2024-02-10 20:39:31 -0800

    add wasm support

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,8 +1,8 @@
 // Top-level build file where you can add configuration options common to all sub-projects/modules.
 plugins {
     id("com.android.application") version "8.2.2" apply false
-    id("org.jetbrains.kotlin.multiplatform") version "1.9.21" apply false
+    id("org.jetbrains.kotlin.multiplatform") version "1.9.22" apply false
     id("com.android.library") version "8.2.2" apply false
-    id("org.jetbrains.compose") version "1.5.11" apply false
+    id("org.jetbrains.compose") version "1.6.0-beta02" apply false
     id("com.vanniktech.maven.publish") version "0.27.0" apply false
 }
