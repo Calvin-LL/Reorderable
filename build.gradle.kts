6536399dc2fdf3b537dfea699a79caef28d8ba35
Calvin Liang <me@calvin.sh>
2024-02-29 20:02:43 -0800

    update org.jetbrains.compose

--- build.gradle.kts
+++ build.gradle.kts
@@ -3,6 +3,6 @@ plugins {
     id("com.android.application") version "8.2.2" apply false
     id("org.jetbrains.kotlin.multiplatform") version "1.9.22" apply false
     id("com.android.library") version "8.2.2" apply false
-    id("org.jetbrains.compose") version "1.6.0-beta02" apply false
+    id("org.jetbrains.compose") version "1.6.0" apply false
     id("com.vanniktech.maven.publish") version "0.27.0" apply false
 }
